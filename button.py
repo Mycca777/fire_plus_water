b181004741fbd06eeb17b003ced87d60739d4f55
LeonardoHammetWalterscott <whalehammet@gmail.com>
2022-12-15 22:36:14 +0300

    Fixed some syntax errors, added some features.

--- button.py
+++ button.py
@@ -13,7 +13,6 @@ class Button:
         self.images = []
         self.images.append(pygame.image.load('button_casual.png').convert_alpha())
         self.images.append(pygame.image.load('button_pressed.png').convert_alpha())
-        self.index = 0
         for i in range(len(self.images)):
             self.images[i] = pygame.transform.scale(self.images[i], (40,10))
         self.button_hitbox = pygame.Rect(self.button_pos[0] + 15, self.button_pos[1] + 13, 30, 10)
@@ -27,16 +26,43 @@ class Button:
         # print(touching)
         if touching:
             self.draw(self.images[1], (self.button_pos[0]-15, self.button_pos[1]-15))
+            return True
         else:
             self.draw(self.images[0], (self.button_pos[0]-15, self.button_pos[1]-15))
+            return False
+
     def draw(self, image, pos):
         self.surface.blit(image, pos)
 
 
 class Platform:
-    def __init__(self, platform_pos, mask, surface):
+    def __init__(self, platform_pos, mask, surface, platform_pos_standart, counter):
         self.surface = surface
-        self.platform_pos = platform_pos[0] - mask.rect.x, platform_pos[1] - mask.rect.y
+        self.platform_pos_standart = list((platform_pos_standart[0] - mask.rect.x, platform_pos_standart[1] - mask.rect.y))
+        self.platform_pos = self.platform_pos_standart
+        self.counter = counter
         self.images = []
         self.images.append(pygame.image.load('platform_casual.png').convert_alpha())
         self.images.append(pygame.image.load('platform_bright.png').convert_alpha())
+        self.platform_hitbox = pygame.Rect(self.platform_pos[0], self.platform_pos[1], 100, 10)
+        self.distance_to_move = 60
+
+    def platform_move(self, button, character):
+        clock = pygame.time.Clock()
+        if button.is_button_pressed(character):
+            if self.counter < self.distance_to_move/5:
+                self.platform_pos[1] += 5*self.counter
+                self.counter += 1
+            else:
+                self.platform_pos[1] = self.platform_pos_standart[1] + self.distance_to_move
+            self.draw(self.images[1], self.platform_pos)
+        else:
+            if self.counter != 0:
+                self.platform_pos[1] += 5*self.counter
+                self.counter -= 1
+            self.draw(self.images[0], self.platform_pos)
+        return self.counter
+
+    def draw(self, image, pos):
+        self.surface.blit(image, pos)
+
--- vis.py
+++ vis.py
@@ -2,9 +2,8 @@
 import pygame
 from light import *
 from flame import *
-from Character import *
+from character import *
 from button import *
-
 
 EMBIANT_COLOR = [(213, 169 + i, 21, 230) for i in range(25)]
 pygame.init()
@@ -15,64 +14,66 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 class Mask:
-    def __init__(self, filename, scale):
-        self.image = pygame.image.load(filename).convert_alpha()
-        self.image.set_colorkey((0,0,0))
-        self.image = pygame.transform.scale(self.image, scale)
-        self.rect = self.image.get_rect(center=(WIDTH / 2, HEIGHT / 2))
-        self.mask = pygame.mask.from_surface(self.image)
+	def __init__(self, filename, scale):
+		self.image = pygame.image.load(filename).convert_alpha()
+		self.image.set_colorkey((0, 0, 0))
+		self.image = pygame.transform.scale(self.image, scale)
+		self.rect = self.image.get_rect(center=(WIDTH / 2, HEIGHT / 2))
+		self.mask = pygame.mask.from_surface(self.image)
 
 
 def put_light_source(surf, mask, pos, color):
-    surf.fill((0,0,0,255))
-    surf.set_alpha(120)
-    cast_all_lights(surf, pos, mask, color)
-    surf.blit(light_surf, (pos[0]-WIDTH / 2, pos[1]-HEIGHT / 2))
-    screen.blit(surf, (0,0))
+	surf.fill((0, 0, 0, 255))
+	surf.set_alpha(120)
+	cast_all_lights(surf, pos, mask, color)
+	surf.blit(light_surf, (pos[0] - WIDTH / 2, pos[1] - HEIGHT / 2))
+	screen.blit(surf, (0, 0))
 
-    
 
 light_surf = pygame.image.load('light_mask.png').convert_alpha()
 light_surf = pygame.transform.scale(light_surf, (WIDTH, HEIGHT))
 
-screen.fill((0,0,0))
+screen.fill((0, 0, 0))
 running = True
-mask = Mask('map.png', (WIDTH, HEIGHT))
+
 all_pos = [(100, 100), (200, 275), (375, 375)]
 flame = [Flame(pos[0], pos[1]) for pos in all_pos]
 motion = None
 character = Character()
 button_pos = (320, 417)
-
+platform_pos_standart = platform_pos = (35, 300)
+counter = 0
 
 while running:
-    screen.fill((0,0,0))
-    surf = pygame.Surface((WIDTH, HEIGHT))
-    surf.set_colorkey((0,0,0))
-    color = character.collision(mask)
-    screen.blit(mask.image, mask.rect)
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            running = False
-        elif e.type == pygame.KEYDOWN:
-            if e.key == pygame.K_LEFT:
-                motion = 'LEFT'
-            elif e.key == pygame.K_RIGHT:
-                motion = 'RIGHT'
-            elif e.key == pygame.K_UP:
-                motion = 'UP'
-            elif e.key == pygame.K_DOWN:
-                motion = 'DOWN'
-        elif e.type == pygame.KEYUP:
-            motion = None
-    # screen.blit(mask.image, character)
-    for i in range(len(all_pos)):
-        put_light_source(surf, mask, all_pos[i], EMBIANT_COLOR[random.randint(1, 15)])
-        flame[i].draw_flame()
+	mask = Mask('map.png', (WIDTH, HEIGHT))
+	screen.fill((0, 0, 0))
+	surf = pygame.Surface((WIDTH, HEIGHT))
+	surf.set_colorkey((0, 0, 0))
+	color = character.collision(mask)
+	screen.blit(mask.image, mask.rect)
+	for e in pygame.event.get():
+		if e.type == pygame.QUIT:
+			running = False
+		elif e.type == pygame.KEYDOWN:
+			if e.key == pygame.K_LEFT:
+				motion = 'LEFT'
+			elif e.key == pygame.K_RIGHT:
+				motion = 'RIGHT'
+			elif e.key == pygame.K_UP:
+				motion = 'UP'
+			elif e.key == pygame.K_DOWN:
+				motion = 'DOWN'
+		elif e.type == pygame.KEYUP:
+			motion = None
+	# screen.blit(mask.image, character)
+	for i in range(len(all_pos)):
+		put_light_source(surf, mask, all_pos[i], EMBIANT_COLOR[random.randint(1, 15)])
+		flame[i].draw_flame()
 
-    character.move(motion)
-    character.draw(surf, screen, color)
-    button = Button(button_pos, mask, screen)
-    button.is_button_pressed(character)
-
-    pygame.display.update()
+	character.move(motion)
+	character.draw(surf, screen, color)
+	button = Button(button_pos, mask, screen)
+	button.is_button_pressed(character)
+	platform = Platform(platform_pos, mask, screen, platform_pos_standart, counter)
+	counter = platform.platform_move(button, character)
+	pygame.display.update()
