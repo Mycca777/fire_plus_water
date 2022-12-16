d82ba3bd1ce9b0ab1fa7910854ffb6e0a765c051
Darya <114287193+dessumt@users.noreply.github.com>
2022-12-16 11:34:53 +0300

    Result

--- button.py
+++ button.py
@@ -6,7 +6,6 @@ import pygame
 
 BUTTON_FILES = ['button_casual.png', 'button_pressed.png']
 
-
 class Button:
     def __init__(self, button_pos, mask, surface):
         self.surface = surface
@@ -15,17 +14,19 @@ class Button:
         self.images.append(pygame.image.load('button_casual.png').convert_alpha())
         self.images.append(pygame.image.load('button_pressed.png').convert_alpha())
         for i in range(len(self.images)):
-            self.images[i] = pygame.transform.scale(self.images[i], (40, 10))
+            self.images[i] = pygame.transform.scale(self.images[i], (40,10))
         self.button_hitbox = pygame.Rect(self.button_pos[0] + 15, self.button_pos[1] + 13, 30, 10)
 
-    def is_button_pressed(self, character):
+    def is_button_pressed(self, character, character1):
         '''
         character has: rect(hitbox), pos(of centre hitbox)
         '''
         character_hitbox = character.hitbox
+        character1_hitbox = character1.hitbox
         touching = pygame.Rect.colliderect(character_hitbox, self.button_hitbox)
+        touching1 = pygame.Rect.colliderect(character1_hitbox, self.button_hitbox)
         # print(touching)
-        if touching:
+        if touching or touching1:
             self.draw(self.images[1], (self.button_pos[0]-15, self.button_pos[1]-15))
             return True
         else:
@@ -45,12 +46,12 @@ class Platform:
         self.images = []
         self.images.append(pygame.image.load('platform_casual.png').convert_alpha())
         self.images.append(pygame.image.load('platform_bright.png').convert_alpha())
-        self.platform_hitbox = pygame.Rect(self.platform_pos[0], self.platform_pos[1], 100, 10)
+        self.platform_hitbox = pygame.Rect(self.platform_pos[0] + 43, self.platform_pos[1] + 23, 40, 10)
         self.distance_to_move = 60
 
-    def platform_move(self, button, character):
+    def platform_move(self, button, character, character1):
         clock = pygame.time.Clock()
-        if button.is_button_pressed(character):
+        if button.is_button_pressed(character, character1):
             if self.counter < self.distance_to_move/5:
                 self.platform_pos[1] += 5*self.counter
                 self.counter += 1
@@ -66,8 +67,7 @@ class Platform:
             self.draw(self.images[0], self.platform_pos)
         return self.counter, self.platform_pos
 
-    def collision(self, character):
-        character_hitbox = character.hitbox
+    def collision(self, character_hitbox):
         touching = pygame.Rect.colliderect(character_hitbox, self.platform_hitbox)
         if touching:
             return True
@@ -76,3 +76,4 @@ class Platform:
 
     def draw(self, image, pos):
         self.surface.blit(image, pos)
+
